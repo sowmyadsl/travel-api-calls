2f9478505ee2c9f44bf351d557a604852a59c355
James Higgins and Sowmya Dinavahi <james+sowmya@>
2017-07-19 11:19:59 -0700

    update api url with deployed heroku link

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ApplicationRecord
    def get_places
-     response = RestClient.get('http://localhost:3001/v1/places')
-    
+     response = RestClient.get('https://james-travel-api.herokuapp.com/v1/places')
+
     JSON.parse(response)
 
    end
